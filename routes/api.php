e0adfaaf89703ab3a9f39cd687915ab5213842cf
duduio2050 <duduio2050@gmail.com>
2024-12-29 16:43:08 +0900

    [FEAT] 센텀 토큰 jwt 토큰 인증 구현

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\UserController;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,36 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/user/register',function (Request $request) {
+    User::create([
+        'email' => $request->input('email'),
+        'password' => bcrypt($request->input('password')),
+        'name' => $request->input('name')
+    ]);
+    return response()->json(['message'=>'success'],200);
+});
+
+
+// 토큰 발급
+Route::post('/user/createToken', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+    // createToken의 인자는 token table의 name 컬럼 값에 저장된다.
+    return "토큰 생성 : ". $user->createToken('token-name')->plainTextToken;
+});
+
+Route::middleware(['api'])->group(function () {
+    Route::middleware(['auth:sanctum'])->group(function () {
+        Route::get('/user/{id}', [UserController::class, 'show'])->name("user.show");
+    });
 });
